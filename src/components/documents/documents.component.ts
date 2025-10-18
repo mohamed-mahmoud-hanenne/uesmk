441b13507d72edcf2eea6668694d8518a2a01569
mohamed mahmoud hanenne <medhanenne38@gmail.com>
2025-10-18 18:52:11 +0100

    corriger btn voir plus doc

--- src/components/documents/documents.component.ts
+++ src/components/documents/documents.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-documents',
@@ -9,16 +10,38 @@ import { Component } from '@angular/core';
 })
 export class DocumentsComponent {
 
-  isOpen: { [key: number]: boolean } = {
-      1: false,
-      2: false,
-      3: false,
-      4: false
-  };
+    // isOpen: { [key: number]: boolean } = {
+    //   1: false,
+    //   2: false,
+    //   3: false,
+    //   4: false,
+    //   5: false,
+    //   6: false,
+    //   7: false,
+    //   8: false
+    // };
 
-toggleIcon(index: number) {
-  this.isOpen[index] = !this.isOpen[index];
-}
+    // toggleIcon(index: number) {
+    //   this.isOpen[index] = !this.isOpen[index];
+    // }
 
+    showList(title: string, items: string[]) {
+    const htmlList = `
+      <ul style="text-align: left; font-size: 0.95rem; line-height: 1.6; margin-left: 10px;">
+        ${items.map(i => `<li style="margin-bottom: 8px;"><i class='bi bi-check-circle-fill text-success me-2'></i>${i}</li>`).join('')}
+      </ul>
+    `;
+
+    Swal.fire({
+      title: `<h4 style="color:#155724; font-weight:700;">${title}</h4>`,
+      html: htmlList,
+      background: '#f9fdf9',
+      confirmButtonText: 'Fermer',
+      confirmButtonColor: '#28a745',
+      width: '42em',
+      showClass: { popup: 'animate__animated animate__fadeInDown' },
+      hideClass: { popup: 'animate__animated animate__fadeOutUp' }
+    });
+  }
 
 }
